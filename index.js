72b101ea6b45b7ed3adb8b29af8264f92fff8a8e
Steven Strumolo <stevenstrumolo@Stevens-Air.home>
2021-01-06 12:43:08 -0500

    updated package.json

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fs = require('fs')
 
 fs.readFile(_dirname + '/info.json', "utf8", function (err, data) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
     return data
-    }
-})
+  }
+});
