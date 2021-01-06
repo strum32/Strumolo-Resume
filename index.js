4675418884cfe8faee8c354862b27f6dbf4aa7fc
Steven Strumolo <stevenstrumolo@Stevens-Air.home>
2021-01-06 12:47:49 -0500

    dirname defined

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-fs.readFile(_dirname + '/info.json', "utf8", function (err, data) {
+fs.readFile(__dirname + '/info.json', "utf8", function (err, data) {
   if (err) {
     console.log(err);
   } else {
